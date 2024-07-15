eb82ac1eec89836465d88bbeacc6d3348515959e
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-07-15 13:35:38 +0600

    'added'

---
+++ db/db.js
@@ -0,0 +1,51 @@
+const ticket = require ('../models/ticket')
+
+class DB {
+    constructor() {
+        this.tickets = []
+    }
+}
+
+// create new ticket
+
+create(){
+
+}
+
+// sell multiple ticket
+
+bulkCreate(){
+
+}
+
+// return all tickets
+
+find (){
+
+}
+
+// single ticket
+
+findById(){
+
+}
+
+// update ticket info
+
+updateById(){
+
+}
+
+// delete ticket from DB
+
+deleteById(){
+
+}
+
+draw (){
+
+}
+
+const DB = new DB()
+
+module.exports = DB 
